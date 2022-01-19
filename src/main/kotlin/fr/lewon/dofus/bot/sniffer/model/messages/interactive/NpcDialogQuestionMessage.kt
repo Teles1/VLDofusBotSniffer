3d0ae508fa6620480a24709e3fce9c554a41bdfd
lewonv <victor.lewon@amersports.com>
2022-01-19 15:39:47 +0100

    New message listened

---
+++ src/main/kotlin/fr/lewon/dofus/bot/sniffer/model/messages/interactive/NpcDialogQuestionMessage.kt
@@ -0,0 +1,8 @@
+package fr.lewon.dofus.bot.sniffer.model.messages.interactive
+
+import fr.lewon.dofus.bot.core.io.stream.ByteArrayReader
+import fr.lewon.dofus.bot.sniffer.model.messages.INetworkMessage
+
+class NpcDialogQuestionMessage : INetworkMessage {
+    override fun deserialize(stream: ByteArrayReader) {}
+}
